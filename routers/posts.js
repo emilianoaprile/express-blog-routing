b8e610c294b4a2cd698f97a1f1303e7f2e214fee
EmilianoAprile <emilianoaprile77@icloud.com>
2024-05-27 15:43:03 +0200

    add > create route

--- routers/posts.js
+++ routers/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const blogController = require("../controllers/BlogController.js");
 
 router.get("/", blogController.index);
+router.get("/create", blogController.create);
 router.get("/:slug", blogController.show);
 
 module.exports = router;
